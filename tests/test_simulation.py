965ab275eec0ec7782ba1372429d8ffdf79dcbb8
developer-hchan <developer.hchan@outlook.com>
2025-09-28 22:39:37 -0400

    Spelling, missed a second spot

--- tests/test_simulation.py
+++ tests/test_simulation.py
@@ -9,33 +9,33 @@ import blackjack as bss
 class CalculateExpectedValue(unittest.TestCase):
     def test_calculate_expected_value_within_range(self):
         """
-        Making sure the calculated expected values is within the minimum and maximum
+        Making sure the calculated expected value is within the minimum and maximum
         possible values.
         """
 
-        # loading a test dictionary from test_data.csv
+        # Loading a test dictionary from test_data.csv
         # NOTE: test_data is not necessarily accurate, it is just used for the testing of inputs and expected outputs
         with open('test_data.csv') as csv_file:
             reader = csv.reader(csv_file)
             test_dictionary = dict(reader)
         
-        # after importing from the test_data.csv, the dictionary is storing the key as a large string, so we need to convert it back to a tuple
+        # After importing from the test_data.csv, the dictionary is storing the key as a large string, so we need to convert it back to a tuple
         for k,v in test_dictionary.items():
             # eval() converts the string-tuple back into a proper tuple
-            # NOTE: by calling global_data_dictionary directly, it shouldn't maintain it's value for the other tests
+            # NOTE: by calling global_data_dictionary directly, it shouldn't maintain its value for the other tests
             bss.global_data_dictionary[eval(k)] = float(v)
         
-        # loading setting.toml
+        # Loading setting.toml
         with open("test_settings.toml", "rb") as f:
             test_settings = tomllib.load(f)
 
-        #output is dictionary where the key is (17, "hard", 5, "stand"), and the value is the expected value (float)
+        # Output is a dictionary where the key is (17, "hard", 5, "stand"), and the value is the expected value (float)
         output = calculate_expected_value(sim_case=(17, "hard", 5, "stand"), toml_settings=test_settings)
 
-        # because because the calculated expected value takes the game.value / number of sims
-        # the max the expected value could be is 25.00; that is if the player wins every match while betting 25.00
+        # Because the calculated expected value takes the game.value / number of sims
+        # The max the expected value could be is 25.00; that is if the player wins every match while betting 25.00
         # 25000.00 / 1000.00 = 25.00
-        # vice versa for minimum value
+        # Vice versa for minimum value
         self.assertGreater(output[(17, "hard", 5, "stand")], -25.00)
         self.assertLess(output[(17, "hard", 5, "stand")], 25.00)
 
